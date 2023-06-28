c470ae04111981dcaa53dc797d3b8bfc8f180dee
Yatsu <yatsu.dev@gmail.com>
2023-06-29 01:46:50 +0200

    example folder add

--- main.py
+++ main.py
@@ -8,43 +8,10 @@ from command_liner import CommandLiner
 commands_prefix = "commandliner"
 command_client = CommandLiner(commands_prefix)
 
-create_command = Command("create", "A command for creating a file").set_help(f"To run the command:\n\t* {commands_prefix} create <file name>").add_parameter("file_name").set_script('''
-file = open("+++", "x")
-file.close()
-''', "file_name")
 
-remove_command = Command("remove", "A command for removing a file").set_help(f"To run the command:\n\t* {commands_prefix} remove <file name>").add_parameter("file_name").set_script('''
-import os
-os.remove("+++")
-''', "file_name")
+def get_prefix():
+    return commands_prefix
 
-rename_command = Command("rename", "A command for renaming a file").set_help(f"To run the command:\n\t* {commands_prefix} rename <file name> <new name>").add_params("file_name", "new_name").set_script('''
-import os
-os.rename("+++", "+++")
-''', "file_name", "new_name")
-
-get_command = Command("get", "A command for perfoming a get request easily.").set_help(f"To run the command:\n\t* {commands_prefix} get <url to get> <file to write data in>").add_params("url", "file_name").set_script('''
-import requests
-r = requests.get("+++")
-data = r.json()
-
-file = open("+++", "a")
-file.write("{}".format(data))
-file.close()
-''', "url", "file_name")
-
-write_command = Command("write", "A command for writing content to a file").set_help(f"To run the command:\n\t* {commands_prefix} write <file to write> <content> (<mode for writing>)").add_params("file_name", "content").add_optional_parameter("mode").set_script('''
-file_name = "+++"
-content = "+++"
-mode = "+++"
-
-if mode is None or mode == "":
-    mode = "w"
-
-file = open(file_name, mode)
-file.write(content)
-file.close()
-''', "file_name", "content", "mode")
 
 # DO NOT REMOVE THIS COMMAND
 # run to create the shell script
@@ -153,7 +120,7 @@ def remove_sh_file():
 remove_sh_file()
 ''')
 
-# first we create the command
+# example command, see more in ./example/commands.py
 example_command = Command("example", "An example command.").set_help(
     f"To run the command:\n\t* example <param1> <param2>").add_params("param1", "param2").add_optional_parameter("param3").set_script('''
 param1 = "+++"
@@ -173,8 +140,7 @@ print(param1)
 ''', "param1", "param2", "param3")
 
 # put all commands here
-commands = list([create_command, remove_command, rename_command,
-                 get_command, write_command, generate_sh_command, destruct_sh_command, example_command])
+commands = list([generate_sh_command, destruct_sh_command, example_command])
 
 # run the script
 command_client.append_commands(
